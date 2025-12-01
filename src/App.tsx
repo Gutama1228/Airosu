09f87df6060fef4a1cd97934590ac1dba81197db
gutama <142733438+Gutama1228@users.noreply.github.com>
2025-12-01 22:30:50 +0700

    update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
+// src/App.tsx
+import { RouterProvider } from 'react-router-dom';
+import appRouter from './app/routes/AppRoutes';
+import { AuthProvider } from './app/providers/AuthProvider';
+import { ThemeProvider } from './app/providers/ThemeProvider';
 
+function App() {
+  return (
+    <ThemeProvider>
+      <AuthProvider>
+        <RouterProvider router={appRouter} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
