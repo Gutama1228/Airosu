2b152c1ae327ecb7b6ebf5b7f0a163872292df14
gutama <142733438+Gutama1228@users.noreply.github.com>
2025-12-01 22:33:33 +0700

    update src/components/layout/AdminLayout.tsx

---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,18 @@
+// src/components/layout/AdminLayout.tsx
+import { Outlet } from 'react-router-dom';
+import { Header } from '../Header';
+import { AdminSidebar } from './AdminSidebar';
+
+export const AdminLayout = () => {
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      <AdminSidebar />
+      <div className="flex flex-col flex-1">
+        <Header />
+        <main className="flex-1 p-4 md:p-8 bg-gray-50">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
