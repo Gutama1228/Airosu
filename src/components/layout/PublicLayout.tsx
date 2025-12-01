56399049ba6a3e69eac6252f2621bce93f0d6544
gutama <142733438+Gutama1228@users.noreply.github.com>
2025-12-01 23:44:51 +0700

    Update PublicLayout.tsx

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,11 +1,10 @@
-// src/components/layout/PublicLayout.tsx
 import { Outlet } from 'react-router-dom';
 import { Header } from './Header';
 import { Footer } from './Footer';
 
 export const PublicLayout = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <main className="flex-1">
         <Outlet />
